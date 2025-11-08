b7474da166729a8bdf31768cf167defb0f2b12fe
Khoi Tran <tranquockhoi1999@gmail.com>
2025-11-09 01:15:46 +0700

    Install vnstock from https://github.com/thinh-vu/vnstock.git

--- vnai/scope/profile.py
+++
@@ -1,9 +0,0 @@
-# Stub file cho vnai.scope.profile
-
-class Inspector:
-    def fingerprint(self):
-        # vnstock chỉ cần gọi để tạo ID máy, nên ta giả vờ trả chuỗi tĩnh
-        return "dummy_fingerprint_1234"
-
-# Tạo biến toàn cục mà vnstock mong đợi
-inspector = Inspector()
