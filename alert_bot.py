acffe9ede74e5d8d1f10bd5e18ab7dae93586cd0
Khoi Tran <tranquockhoi1999@gmail.com>
2025-11-09 17:46:28 +0700

    test push git to 2 account github

--- alert_bot.py
+++ alert_bot.py
@@ -3680,7 +3680,7 @@ async def asgi_wrapper_app(scope, receive, send):
     # 🚀 BIẾN MỚI: Khai báo để sử dụng
     global BACKGROUND_TASKS, MAIN_LOOP
     global ADMIN_ID, initial_active, INSTANCE_ID
-
+ 
     if scope["type"] == "lifespan":
         while True:
             message = await receive()
@@ -3703,7 +3703,7 @@ async def asgi_wrapper_app(scope, receive, send):
                 log.info("[Lifespan] Đang chờ 60s để Render ổn định health check...")
                 await asyncio.sleep(60) 
                 # ==========================================================
- 
+
                 # (Lấy toàn bộ các loop từ hàm main() chuyển lên đây)
                 BACKGROUND_TASKS = [
                     MAIN_LOOP.create_task(alert_loop()),
